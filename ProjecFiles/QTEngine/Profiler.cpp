373a1389bcbc3d6f65abf8d0a05000b63c6a852a
Meta XI <meta-xi@hotmail.com>
2017-11-21 18:40:36 +0100

    Adding some namespaces

--- ProjecFiles/QTEngine/Profiler.cpp
+++ ProjecFiles/QTEngine/Profiler.cpp
@@ -1,90 +1,96 @@
 #include "Profiler.h"
 #ifdef PROFILING_ON
-Profiler Profiler::theInstance;
-
-Profiler& Profiler::getInstance() {
-	return theInstance;
-}
-
 #include <fstream>
+#endif // PROFILING_ON
 
-static std::ofstream outStream;
-using std::ios;
+namespace Profiling {
 
-void Profiler::initialize(const char* fileName) {
-	this->fileName = fileName;
-	frameIndex = -1;
-	categoryIndex = 0;
-	numUsedCategories = 0;
-}
+#ifdef PROFILING_ON
+	Profiler Profiler::theInstance;
 
-void Profiler::shutdown() {
-	writeData();
-}
-
-char Profiler::getDelimiter(unsigned int index) const {
-	return ((index + 1) < numUsedCategories ? ',' : '\n');
-}
-
-bool Profiler::wrapped() const {
-	return frameIndex >= MAX_FRAME_SAMPLES && frameIndex != -1;
-}
-
-void Profiler::writeData() const {
-	outStream.open(fileName, ios::trunc);
-	//headers
-	for (unsigned int i = 0; i < numUsedCategories; i++) {
-		outStream << categories[i].name;
-		outStream << getDelimiter(i);
+	Profiler& Profiler::getInstance() {
+		return theInstance;
 	}
 
-	unsigned int endIndex;
-	unsigned int startIndex;
-	if (wrapped()) {
-		endIndex = frameIndex % MAX_FRAME_SAMPLES;
-		startIndex = (endIndex + 1) % MAX_FRAME_SAMPLES;
-		while (startIndex != endIndex) {
-			writeFrame(startIndex);
-			startIndex = (startIndex + 1) % MAX_FRAME_SAMPLES;
+	static std::ofstream outStream;
+	using std::ios;
+
+	void Profiler::initialize(const char* fileName) {
+		this->fileName = fileName;
+		frameIndex = -1;
+		categoryIndex = 0;
+		numUsedCategories = 0;
+	}
+
+	void Profiler::shutdown() {
+		writeData();
+	}
+
+	char Profiler::getDelimiter(uint index) const {
+		return ((index + 1) < numUsedCategories ? ',' : '\n');
+	}
+
+	bool Profiler::wrapped() const {
+		return frameIndex >= MAX_FRAME_SAMPLES && frameIndex != -1;
+	}
+
+	void Profiler::writeData() const {
+		outStream.open(fileName, ios::trunc);
+		//headers
+		for (uint i = 0; i < numUsedCategories; i++) {
+			outStream << categories[i].name;
+			outStream << getDelimiter(i);
 		}
-		writeFrame(startIndex);
+
+		uint endIndex;
+		uint startIndex;
+		if (wrapped()) {
+			endIndex = frameIndex % MAX_FRAME_SAMPLES;
+			startIndex = (endIndex + 1) % MAX_FRAME_SAMPLES;
+			while (startIndex != endIndex) {
+				writeFrame(startIndex);
+				startIndex = (startIndex + 1) % MAX_FRAME_SAMPLES;
+			}
+			writeFrame(startIndex);
+		}
+		else {
+			uint numActualFrames = frameIndex;
+			if (currentFrameComplete())
+				numActualFrames++;
+			startIndex = 0;
+			endIndex = numActualFrames;
+			while (startIndex < endIndex)
+				writeFrame(startIndex++);
+		}
+		outStream.close();
 	}
-	else {
-		unsigned int numActualFrames = frameIndex;
-		if (currentFrameComplete())
-			numActualFrames++;
-		startIndex = 0;
-		endIndex = numActualFrames;
-		while (startIndex < endIndex)
-			writeFrame(startIndex++);
+
+	void Profiler::newFrame() {
+		frameIndex++;
+		categoryIndex = 0;
 	}
-	outStream.close();
-}
 
-void Profiler::newFrame() {
-	frameIndex++;
-	categoryIndex = 0;
-}
+	void Profiler::addEntry(const char* category, float time) {
 
-void Profiler::addEntry(const char* category, float time) {
-
-	ProfileCategory& pc = categories[categoryIndex++];
-	if (frameIndex == 0) {
-		pc.name = category;
-		numUsedCategories++;
+		ProfileCategory& pc = categories[categoryIndex++];
+		if (frameIndex == 0) {
+			pc.name = category;
+			numUsedCategories++;
+		}
+		pc.samples[frameIndex % MAX_FRAME_SAMPLES] = time;
 	}
-	pc.samples[frameIndex % MAX_FRAME_SAMPLES] = time;
-}
 
-void Profiler::writeFrame(unsigned int frameNumber) const {
-	for (unsigned int cat = 0; cat < numUsedCategories; cat++) {
-		outStream << categories[cat].samples[frameNumber];
-		outStream << getDelimiter(cat);
+	void Profiler::writeFrame(uint frameNumber) const {
+		for (uint cat = 0; cat < numUsedCategories; cat++) {
+			outStream << categories[cat].samples[frameNumber];
+			outStream << getDelimiter(cat);
+		}
 	}
-}
 
-bool Profiler::currentFrameComplete() const {
-	return categoryIndex == numUsedCategories;
-}
+	bool Profiler::currentFrameComplete() const {
+		return categoryIndex == numUsedCategories;
+	}
 
 #endif
+}
+
--- ProjecFiles/QTEngine/Profiler.h
+++ ProjecFiles/QTEngine/Profiler.h
@@ -1,44 +1,47 @@
 #pragma once
+#include <TypeDefs.h>
 
-class __declspec(dllexport) Profiler {
-public: 
-	static const unsigned int MAX_FRAME_SAMPLES = 1000;
+namespace Profiling {
 
-private:
-	Profiler() {}
-	Profiler(const Profiler&);
-	Profiler& operator=(const Profiler&);
-	static Profiler theInstance;
-	static Profiler& getInstance();
+	class __declspec(dllexport) Profiler {
+	public:
+		static const uint MAX_FRAME_SAMPLES = 1000;
 
-#if PROFILING_ON
-	const char* fileName;
-	static const unsigned int MAX_PROFILE_CATEGORIES = 20;
-	unsigned int frameIndex;
-	unsigned int categoryIndex;
-	unsigned int numUsedCategories;
-		
-	struct ProfileCategory{
-		const char* name;
-		float samples[MAX_FRAME_SAMPLES];
-	} categories[MAX_PROFILE_CATEGORIES];
-	bool currentFrameComplete() const;
-	void writeData() const;
-	void writeFrame(unsigned int frameNumber) const;
-	char getDelimiter(unsigned int index) const;
-	bool wrapped() const;
-#endif
-public:
-#if PROFILING_ON
-	void initialize(const char* fileName);
-	void shutdown();
-	void newFrame();
-	void addEntry(const char* category, float time);
-#else
-	void initialize(const char* fileName){}
-	void shutdown(){}
-	void newFrame(){}
-	void addEntry(const char* category, float time){}
-#endif
-};
+	private:
+		Profiler() {}
+		Profiler(const Profiler&);
+		Profiler& operator=(const Profiler&);
+		static Profiler theInstance;
+		static Profiler& getInstance();
 
+	#if PROFILING_ON
+		const char* fileName;
+		static const uint MAX_PROFILE_CATEGORIES = 20;
+		uint frameIndex;
+		uint categoryIndex;
+		uint numUsedCategories;
+
+		struct ProfileCategory {
+			const char* name;
+			float samples[MAX_FRAME_SAMPLES];
+		} categories[MAX_PROFILE_CATEGORIES];
+		bool currentFrameComplete() const;
+		void writeData() const;
+		void writeFrame(uint frameNumber) const;
+		char getDelimiter(uint index) const;
+		bool wrapped() const;
+	#endif
+	public:
+	#if PROFILING_ON
+		void initialize(const char* fileName);
+		void shutdown();
+		void newFrame();
+		void addEntry(const char* category, float time);
+	#else
+		void initialize(const char* fileName) {}
+		void shutdown() {}
+		void newFrame() {}
+		void addEntry(const char* category, float time) {}
+	#endif
+	};
+}
